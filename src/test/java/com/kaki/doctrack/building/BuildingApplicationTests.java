d613f44aff8cb45f99acd941f74f0e91cef0aa74
Joaquin Navarro <alfian1991@gmail.com>
2024-09-04 12:08:44 -0400

    building-service

---
+++ src/test/java/com/kaki/doctrack/building/BuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaki.doctrack.building;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
